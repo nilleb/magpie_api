f4aa79817c8ac9b1f2377fabf50f48ba4030b50b
Ivo Bellin Salarin <me@nilleb.com>
2020-12-19 19:53:41 +0100

    fix: serve a static html page and validate swagger

---
+++ magpie_server/references.py
@@ -0,0 +1,8 @@
+from magpie.app import configuration
+from magpie.plugins.dbadapter import ReferenceData, DBProvider
+
+def list():
+    db = DBProvider(configuration()).database
+    ReferenceData.bind(db)
+    query = ReferenceData.select().order_by(ReferenceData.collected_at)
+    return [item for item in query]
