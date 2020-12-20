cc57bc6c95332d751d7632ee238a1e2ae96f4a1a
Ivo Bellin Salarin <me@nilleb.com>
2020-12-20 11:08:35 +0100

    feat: retrieve references

--- client/src/components/Ping.vue
+++ client/src/components/Ping.vue
@@ -1,16 +1,35 @@
 <template>
   <div>
+    <p>Hello!</p>
     <p>{{ msg }}</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Ping',
   data() {
     return {
-      msg: 'Hello!',
+      msg: '',
     };
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:5000/api/v1/references';
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
   },
 };
 </script>
